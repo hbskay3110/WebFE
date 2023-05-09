477147d2d6dbcc60cef51884f148c85fee5b713e
PhanThyAn <phanthyan123@gmail.com>
2023-05-09 08:25:54 -0400

    login and register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Component/Header";
+import Login from './Component/Login';
+import Register from './Component/Register';
 
 function App() {
   return (
-    <Header></Header>
+   <Register></Register>
 
   );
 }
