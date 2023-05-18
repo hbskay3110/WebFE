3802e51c7cedc250ec3ca688c7ed33e270204823
nhutHao02 <trannhuthao091@gmail.com>
2023-05-19 01:09:33 +0700

    cập nhật

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Component/Header";
-<<<<<<< HEAD
 import ProductDetail from './Component/Product-detail';
 import ProductList from './Component/Product-list';
 
@@ -9,14 +8,6 @@ import ProductList from './Component/Product-list';
 function App() {
   return (
     <ProductList/>
-=======
-import Login from './Component/Login';
-import Register from './Component/Register';
-
-function App() {
-  return (
-   <Register></Register>
->>>>>>> 9317df37f0ff1a59129b3353b9e49d99a0b5061f
 
   );
 }
